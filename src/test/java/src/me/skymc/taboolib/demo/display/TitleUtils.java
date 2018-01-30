9b1ccec5bba5289f509c95484927b134900e510d
坏黑 <sky@asgard.me>
2018-01-30 13:31:28 +0800

    Title & ActionBar

---
+++ src/test/java/src/me/skymc/taboolib/demo/display/TitleUtils.java
@@ -0,0 +1,30 @@
+package me.skymc.taboolib.demo;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import me.skymc.taboolib.display.ActionUtils;
+import me.skymc.taboolib.display.TitleUtils;
+
+public class DisplayDemo implements Listener {
+	
+	@EventHandler
+	public void onCommand(PlayerJoinEvent e) {
+		/**
+		 * 向玩家发送大标题
+		 */
+		TitleUtils.sendTitle(e.getPlayer(), "欢迎!", 10, 100, 10, "欢迎来到这个服务器!", 10, 100, 10);
+		
+		/**
+		 * 向其他玩家发送小标题
+		 */
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if (!player.equals(e.getPlayer())) {
+				ActionUtils.send(player, "玩家 " + e.getPlayer().getName() + " 加入服务器");
+			}
+		}
+	}
+}
